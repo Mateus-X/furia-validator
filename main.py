7b22efbe29a9647615d454a263af05501ac2186e
mateus-x <mateusdecastroxavier@gmail.com>
2025-05-03 23:12:13 -0300

    feat/first-commit

---
+++ main.py
@@ -0,0 +1,116 @@
+from json import JSONEncoder
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from mistralai import *
+import os
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from enum import Enum
+from pathlib import Path
+import base64
+
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
+
+API_KEY = os.getenv("MISTRAL_API_KEY")
+
+# Modelo que permite OCR
+model = "mistral-ocr-latest"
+
+client = Mistral(api_key=API_KEY)
+
+class Document(BaseModel):
+    Name: str
+    DocumentType: str
+    DocumentNumber: str
+    SeemsValid: bool
+
+
+class StructuredOCR(BaseModel):
+    FileName: str 
+    OcrContents: Document
+
+class ValidateRequest(BaseModel):
+    image_path: str
+
+@app.get("/")
+async def root():
+    return {"message": "API Furia"}
+
+@app.post("/validate")
+async def validate(request: ValidateRequest) -> StructuredOCR:
+    image_file = Path(request.image_path)
+    if not image_file.is_file():
+        raise HTTPException(
+            status_code=500, 
+            detail=f"Arquivo não encontrado."
+        )
+
+    if not image_file.suffix in [".jpg", ".jpeg", ".png"]: 
+        raise HTTPException(
+            status_code=400, 
+            detail=f"Formato do arquivo não suportado. Formatos suportados: jpg, jpeg, png"
+        )
+    
+
+
+    encoded_image = base64.b64encode(image_file.read_bytes()).decode()
+    base64_data_url = f"data:image/jpeg;base64,{encoded_image}"
+
+    image_response = client.ocr.process(
+        document=ImageURLChunk(image_url=base64_data_url),
+        model="mistral-ocr-latest"
+    )
+
+    print(image_response)
+
+    image_ocr_markdown = image_response.pages[0].markdown
+
+    chat_response = client.chat.parse(
+        model="pixtral-12b-latest",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    ImageURLChunk(image_url=base64_data_url),
+                    TextChunk(text=(
+                        f"This is the image's OCR in markdown:\n{image_ocr_markdown}\n.\n"
+                        "Convert this into a structured JSON response "
+                        "with the OCR contents in a sensible dictionnary\n"
+                        "Also return a boolean field 'seemsValid' that indicates if the picture has a document structure pattern."
+                        )
+                    )
+                ]
+            }
+        ],
+        response_format=StructuredOCR,
+        temperature=0
+    )
+
+    return chat_response.choices[0].message.parsed
+
+
+@app.get("/ping")
+async def ping():
+    try:
+        chat_response = client.chat.complete(
+            model= model,
+            temperature=0.1,
+            messages = [
+                {
+                    "role": "user",
+                    "content": "Forma comum de testar apis, eu mando ping e voce responde pong",
+                },
+            ]
+        )
+        return chat_response.choices[0].message.content
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
